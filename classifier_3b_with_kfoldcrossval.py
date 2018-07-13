31be0aa670243801a7682614f7dcd63680a9d3d2
previtus <previtus@gmail.com>
2018-07-12 23:10:28 -0400

    Now with k-fold crossvalidation among the examples...

---
+++ classifier_3b_with_kfoldcrossval.py
@@ -0,0 +1,304 @@
+# SETUP
+
+img_size = None #(20,20)
+img_size = (150,150)
+epochs = 200
+batch_size = 16
+
+# 4-folds: train 2211 , test 737
+# 3-folds: train 1965 , test 983
+k_folds = 3
+
+#(instead of)validation_split = 0.3
+
+
+RESCALE = 1. / 255 # put data from 0-255 into 0-1
+#RESCALE = 0.004
+#RESCALE = 0.01
+#RESCALE = 0
+
+# GET ALL DATA
+# define the classes in here directly
+
+FOLDER = 'saved_images_square_224'
+#FOLDER = 'saved_images_square_20'
+
+classes_names = ['lp', 'tc', 'tr', 'vt']
+num_classes = len(classes_names)
+folders = ['data/'+FOLDER+'/train/lp/', 'data/'+FOLDER+'/train/tc/', 'data/'+FOLDER+'/train/tr/', 'data/'+FOLDER+'/train/vt/',
+           'data/' + FOLDER + '/test/lp/', 'data/' + FOLDER + '/test/tc/', 'data/' + FOLDER + '/test/tr/',
+           'data/' + FOLDER + '/test/vt/',
+        ]
+labels_texts = classes_names+classes_names
+labels = [0, 1, 2, 3, 0, 1, 2, 3]
+
+SHUFFLE_SEED=1337
+
+
+############ Whats bellow doesn't have to be changed dramatically
+
+X_all_paths = []
+Y_all_labels = []
+
+from os import listdir
+from os.path import isfile, join
+from keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+import random
+import keras
+from matplotlib import pyplot as plt
+
+
+def load_images_with_keras(img_paths, target_size=None):
+    imgs_arr = [img_to_array(load_img(path, target_size=target_size)) for path in img_paths]
+    imgs_arr = np.array(imgs_arr)
+    return imgs_arr
+
+for i,folder in enumerate(folders):
+    onlyfiles = [f for f in listdir(folder) if isfile(join(folder, f))]
+    onlyfiles = sorted(onlyfiles)
+    label = labels[i]
+    #print(len(onlyfiles), "loaded", labels_texts[i], labels[i])
+    #print(folder+onlyfiles[0])
+    #print(folder+onlyfiles[-1])
+    paths = [folder+file for file in onlyfiles]
+    Y_all_labels += [label]*len(paths)
+    X_all_paths += paths
+
+X_all_image_data = load_images_with_keras(X_all_paths, target_size=img_size)
+Y_all_labels = np.array(Y_all_labels)
+
+Y_all_labels = keras.utils.to_categorical(Y_all_labels, num_classes=num_classes)
+
+print("X_all_image_data:", X_all_image_data.shape)
+print("Y_all_labels:", Y_all_labels.shape)
+print("---")
+
+VIZ = False
+if VIZ:
+    images = range(0,9)
+    for i in images:
+        plt.subplot(330 + 1 + i)
+        plt.imshow(X_all_image_data[i])
+    #Show the plot
+    plt.show()
+
+
+VIZ = False
+if VIZ:
+    plt.hist(Y_all_labels, alpha=0.5)
+    plt.title('Number of examples from each class')
+    plt.xticks(np.arange(len(classes_names)), classes_names)
+    plt.ylabel('count')
+    plt.show()
+
+# NOW WE HAVE ALL THE DATA X AND THEIR LABELS Y IN X_all_image_data, Y_all_labels
+def shuffle_two_lists_together(a,b, SEED=None):
+    combined = list(zip(a, b))
+    if SEED is not None:
+        random.seed(SEED)
+    random.shuffle(combined)
+    a[:], b[:] = zip(*combined)
+    return a,b
+
+def split_data(x,y,validation_split=0.2):
+    split_at = int(len(x) * (1 - validation_split))
+    x_train = x[0:split_at]
+    y_train = y[0:split_at]
+    x_test = x[split_at:]
+    y_test = y[split_at:]
+
+    print("Split", len(x), "images into", len(x_train), "train and", len(x_test), "test sets.")
+    return x_train,y_train,x_test,y_test
+
+def chunks(l, k):
+    ''' Chunk data from list l into k fjords. Not randomizing the order. '''
+    # the first chunk may have more data (if len(l) is not divisible by k)
+    # if len(l) < k then the last chunks will be empty
+    a = np.array_split(np.array(l), k)
+    b = []
+    for i in a:
+        b.append(i.tolist())
+    return b
+
+def kfoldcrossval(all_x, all_y, k, shuffle=True):
+    # split all_x, all_y into k folds
+    # return array of k times [train_x, train_y, test_x, test_y] (test or val)
+    # (... arguably this is a bit slow, it would be faster to handle indices)
+
+    # at start we would like to shuffle the two arrays
+    if shuffle:
+        all_x, all_y = shuffle_two_lists_together(all_x, all_y, SEED=SHUFFLE_SEED)
+
+    # prepare chunks
+    all_x_in_chunks = chunks(all_x, k)
+    all_y_in_chunks = chunks(all_y, k)
+
+    # now build the array [ [train_x, train_y, test_x, test_y], ... ]
+    kfolds_in_array = []
+
+    for i in range(0, k):
+        # i-th chunk should be the test data, the rest should serve as train data
+        test_x = all_x_in_chunks[i]
+        test_y = all_y_in_chunks[i]
+
+        train_x = []
+        train_y = []
+
+        for j in range(0,len(all_x_in_chunks)):
+            if j != i:
+                train_x += all_x_in_chunks[j]
+                train_y += all_y_in_chunks[j]
+
+        # np or not np lists omg
+        #train_x = [np.asarray(subset) for subset in train_x]
+        #train_y = [np.asarray(subset) for subset in train_y]
+        #test_x = [np.asarray(subset) for subset in test_x]
+        #test_y = [np.asarray(subset) for subset in test_y]
+
+        kfolds_in_array.append([train_x, train_y, test_x, test_y])
+
+    return kfolds_in_array
+
+
+# GOTTA GO FAST
+##X_all_image_data = X_all_image_data[0:100]
+##Y_all_labels = Y_all_labels[0:100]
+
+# RESCALE image data, from 0-255 to 0-1
+X_all_image_data *= RESCALE
+
+#x = np.asarray(range(0,10))
+#y = np.asarray(range(0,10))
+#k = 5
+#print("x:",x)
+#kfolds_in_array = kfoldcrossval(x, y, k)
+#for c in kfolds_in_array:
+#    print("train",len(c[0]),"=",c[0], "test",len(c[2]),"=",c[2], )
+
+kfolds_in_array = kfoldcrossval(X_all_image_data,Y_all_labels, k_folds)
+
+for fold in kfolds_in_array:
+    [x_train, y_train, x_test, y_test] = fold
+    print("train", len(y_train), ", test", len(y_test))
+
+print("---")
+
+# NOW REPEAT THIS FOR EVERY FOLD:
+#
+# (reset model)
+# 1.) train model on train_x, train_y for certain number of epoch
+#     - you can save history
+# 2.) measure accuracy and loss on test_x, test_y
+#     -> accuracy_i, loss_i
+# (repeat for each fold)
+#
+# Calculate mean accuracy and mean loss
+# Possibly plot all the histories together
+
+ACCURACIES = []
+LOSSES = []
+HISTORIES = []
+
+for fold in kfolds_in_array:
+    [x_train, y_train, x_test, y_test] = fold
+
+    x_train = np.asarray(x_train)
+    y_train = np.asarray(y_train)
+    x_test = np.asarray(x_test)
+    y_test = np.asarray(y_test)
+
+    print("x_test:", x_train.shape)
+    print("y_test:", y_train.shape)  # , y_train[0:10])
+    print("x_test:", x_test.shape)
+    print("y_test:", y_test.shape)  # , y_test[0:10])
+    print("-")
+
+    """
+    VIZ = False
+    if VIZ:
+        images = range(0,9)
+        for i in images:
+            plt.subplot(330 + 1 + i)
+            plt.imshow(x_train[i])
+        #Show the plot
+        plt.show()
+
+    # See how is the distribution in test and train (should be the same)
+    VIZ = False # PS: this works only without the categorical data...
+    if VIZ:
+        from matplotlib import pyplot as plt
+        plt.subplot(2, 1, 1)
+        plt.hist(y_train, alpha=0.5)
+        plt.title('Train data classes')
+        plt.xticks(np.arange(len(classes_names)), classes_names)
+        plt.ylabel('count')
+
+        plt.subplot(2, 1, 2)
+        plt.hist(y_test, alpha=0.5)
+        plt.title('Test data classes')
+        plt.xticks(np.arange(len(classes_names)), classes_names)
+        plt.ylabel('count')
+        plt.show()
+    """
+    # Now for the model
+
+    #continue
+
+    from keras import applications
+    model = applications.VGG16(include_top=False, weights='imagenet')
+
+    # HERE WE ACTUALLY HAVE TO EDIT THE DATA OURSELF,
+    # predict(self, x, batch_size=None, verbose=0, steps=None)
+    X_bottleneck_train = model.predict(x_train)
+    X_bottleneck_test = model.predict(x_test)
+
+    print("X_bottleneck_train:", X_bottleneck_train.shape)
+    print("y_test:", y_train.shape)#, y_train[0:10])
+    print("X_bottleneck_test:", X_bottleneck_test.shape)
+    print("y_test:", y_test.shape)#, y_test[0:10])
+    print("-")
+
+    print("train_data.shape[1:]", X_bottleneck_train.shape[1:])
+
+    from keras.models import Sequential
+    from keras.layers import Dropout, Flatten, Dense
+
+    model = Sequential()
+    model.add(Flatten(input_shape=X_bottleneck_train.shape[1:]))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='sigmoid'))
+
+    #model.summary()
+
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    history = model.fit(X_bottleneck_train, y_train,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        validation_data=(X_bottleneck_test, y_test),
+                        verbose=0)
+
+    scores = model.evaluate(X_bottleneck_test, y_test, verbose=0)
+    loss, accuracy = scores
+
+    print("scores", scores)
+
+    ACCURACIES.append(accuracy)
+    LOSSES.append(loss)
+    HISTORIES.append(history.history)
+
+print("ACCURACIES", ACCURACIES)
+print("LOSSES", LOSSES)
+
+print("Accuracy %.4f%% (+/- %.2f%%)" % (np.mean(ACCURACIES), np.std(ACCURACIES)))
+print("Loss %.4f (+/- %.2f%%)" % (np.mean(LOSSES), np.std(LOSSES)))
+
+
+from visualize_history import visualize_histories, visualize_special_histories
+#visualize_histories(HISTORIES, show_also='acc', save=True, save_path='classifier3b_'+str(epochs)+'epochs_'+str(k_folds)+'folds_')
+
+# def visualize_special_histories(histories, plotvalues='loss', show=True, save=False, save_path='', custom_title=None, just_val=False):
+visualize_special_histories(HISTORIES, plotvalues='acc', save=True, save_path='classifier3b_'+str(epochs)+'epochs_'+str(k_folds)+'folds_')
+
--- visualize_history.py
+++ visualize_history.py
@@ -35,9 +35,9 @@ def visualize_history(hi, show=True, save=False, save_path='', show_also='', cus
     plt.xlabel('epoch')
 
     if show_also == '':
-        plt.legend(['train', 'test'], loc='upper left')
+        plt.legend(['train', 'test'], loc='best')
     else:
-        plt.legend(['train', 'test', 'train-'+show_also, 'test-'+show_also], loc='upper left')
+        plt.legend(['train', 'test', 'train-'+show_also, 'test-'+show_also], loc='best')
 
 
     if save:
@@ -57,5 +57,97 @@ def visualize_history(hi, show=True, save=False, save_path='', show_also='', cus
     if show:
         plt.show()
 
+    #plt.clf()
+    return plt
+
+def visualize_histories(histories, show=True, save=False, save_path='', show_also='', custom_title=None):
+    for hi in histories[:-1]:
+        visualize_history(hi, show=False, save=False, save_path='', show_also=show_also, custom_title=None)
+
+    plt = visualize_history(histories[-1], show=show, save=save, save_path=save_path, show_also=show_also, custom_title=custom_title)
+    plt.clf()
+    return 0
+
+def visualize_special_histories(histories, plotvalues='loss', show=True, save=False, save_path='', custom_title=None, just_val=False):
+    '''
+    We are visualizing results of a k-fold crossvalidation training. In <histories> we have the individual runs of the experiment.
+    '''
+
+    train_color = 'grey'
+    val_color = 'blue'
+
+    avg_train_color = 'red'
+    avg_val_color = 'green'
+
+    avg_train = []
+    avg_val = []
+
+    # count the averages
+
+    epochs = len(histories[0][plotvalues])
+    for epoch in range(0, epochs):
+        trains = []
+        vals = []
+        for hi in histories:
+            train = hi[plotvalues][epoch]
+            val = hi['val_'+plotvalues][epoch]
+            trains.append(train)
+            vals.append(val)
+        avg_train.append( np.mean(trains) )
+        avg_val.append( np.mean(vals) )
+
+    import matplotlib.pyplot as plt
+    plt.figure()
+
+    if custom_title is None:
+        custom_title = 'model ' + plotvalues
+    if just_val:
+        custom_title = custom_title + ' (just validation results)'
+
+    i = 0
+    leg = []
+    if not just_val:
+        leg.append('average training')
+    leg.append('average validation')
+
+    if not just_val:
+        leg.append('training errors')
+    leg.append('validation errors')
+
+    # now averages:
+    if not just_val:
+        plt.plot(avg_train, color=avg_train_color)
+    plt.plot(avg_val, color=avg_val_color)
+
+    for hi in histories:
+        # list all data in history
+        print(hi.keys())
+        # summarize history for loss
+        if not just_val:
+            plt.plot(hi[plotvalues], linestyle='dashed', color=train_color)
+        plt.plot(hi['val_'+plotvalues], linestyle='dashed', color=val_color)
+        i += 1
+
+    # OK, but we also want these on top...:
+    if not just_val:
+        plt.plot(avg_train, color=avg_train_color)
+    plt.plot(avg_val, color=avg_val_color)
+
+
+    plt.title(custom_title)
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+
+
+    #plt.legend(leg, loc='lower left')
+    plt.legend(leg, loc='best')
+    if save:
+        plt.savefig(save_path) #+plotvalues+'.png')
+        plt.savefig(save_path+'.pdf', format='pdf')
+
+    if show:
+        plt.show()
+
     plt.clf()
     return plt
+
